59f11d7cbf001dce7d524bfab6f667f3e704feec
boros <borosadam22gmail.com>
2023-06-10 23:28:47 +0100

    Added another message.

--- src/hu/ak_akademia/scope/Main.java
+++ src/hu/ak_akademia/scope/Main.java
@@ -3,7 +3,7 @@ package hu.ak_akademia.scope;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Scope Team!!");
-        System.out.println("Have a nice day!");
+        System.out.println("Have a nice day!!");
 
     }
 }
